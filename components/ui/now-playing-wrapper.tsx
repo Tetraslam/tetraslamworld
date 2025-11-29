1cde832c32794cc2a91bc58077f0af457ddea704
Tetraslam <bhowmickshresht@gmail.com>
2025-11-29 02:43:02 -0500

    rewrite

--- components/ui/now-playing-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-
-const NowPlayingDynamic = dynamic(() => import('@/app/components/now-playing').then(m => m.NowPlaying), { ssr: false });
-
-export default function NowPlayingWrapper() {
-  return <NowPlayingDynamic />;
-} 
--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://tetraslam.world',
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-  exclude: ['/api/*'],
-}; 
